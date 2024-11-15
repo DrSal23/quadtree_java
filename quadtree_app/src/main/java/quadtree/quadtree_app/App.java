7ce920441c35b459beb4134574532d6579a97782
Salah Mahmoud <smah1219@students.vsu.edu>
2024-11-15 16:38:53 -0500

    Added example.cmmd and App.java prep for reading from a file

--- quadtree_app/src/main/java/quadtree/quadtree_app/App.java
+++ quadtree_app/src/main/java/quadtree/quadtree_app/App.java
@@ -1,10 +1,30 @@
 package quadtree.quadtree_app;
 
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * Hello world!
  */
 class App{
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//System.out.println("Hello World!");
+		if(args.length == 0) {
+			System.out.println("No arguments");
+			return;
+		}
+		
+		try {
+			Scanner sc = new Scanner(new File(args[0]));
+			// implement reading input
+			System.out.println(args[0]);
+			
+			sc.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
